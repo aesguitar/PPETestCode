cb38c3e7269b3cc989bf91ac18f1bbdaa9107249
aesguitar <aesguitar@gmail.com>
2020-05-07 12:23:02 -0400

    SerialWriter.py updated to write to serial port at the correct rate of (8 * 1/9600) seconds per character

--- Code/SerialWriter.py
+++ Code/SerialWriter.py
@@ -1,12 +1,20 @@
 import serial
 from random import uniform
+from time import sleep
 
+# ***********NOTE************
+# run 'socat -d -d pty,raw,echo=0,b9600 pty,raw,echo=0,b9600'
+# before executing. This sets up a serial port this program
+# can write to in order to emulate the arduino output to the
+# pi.
 rrange = [15, 25]
 
 
 def openserialport():
     serport = "/dev/pts/2"
-    ser = serial.Serial(serport, 9600, rtscts=True, dsrdtr=True)
+    ser = serial.Serial(serport, 9600)
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
     return ser
 
 
@@ -26,15 +34,20 @@ def readsensor(sensors, number):
 
 
 def main():
+    numsensors = 8
     ser = openserialport()
-    sensors = createsensors(8)
-    loops = int(len(sensors) * 3)
+    sensors = createsensors(numsensors)
+    loops = int(len(sensors) * 200)
     for i in range(loops):
-        ind = (i % 7) + 1
+        ind = (i % numsensors) + 1
         sensors[ind] = readsensor(sensors, ind)
         payload = str(ind) + ":" + str(sensors[ind]) + '\n'
-        # print(payload)
-        ser.write(bytearray(payload, encoding='utf-8'))
+        #print(str(datetime.now()) + ": " + payload)
+        data = bytearray(payload, encoding='utf-8')
+        for d in data:
+            db = d.to_bytes(1, 'big')
+            ser.write(db)
+            sleep(1/9600*8)
     ser.close()
 
 
