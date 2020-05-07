50d8b37c80a33b768245007be99e6aebe907275f
aesguitar <aesguitar@gmail.com>
2020-05-07 12:37:25 -0400

    FileIO reads serial input and enumerates sensors found in serial stream

--- Code/FileIO.py
+++ Code/FileIO.py
@@ -3,12 +3,38 @@ import serial
 
 
 def openserialport(portname):
-    ser = serial.Serial(portname, 9600, rtscts=True, dsrdtr=True)
+    ser = serial.Serial(portname, 9600, rtscts=True, dsrdtr=True, timeout=1)
+    ser.reset_input_buffer()
+    return ser
+
+
+# clears input buffer, reads in the current line
+def readnewestline(ser):
+    ser.reset_input_buffer
+    ser.readline()
+    return ser.readline()
 
 
 def main():
     ser = openserialport('/dev/pts/1')
+    sensors = {"0": "0"}
+    # enumerate sensors
+    loops = 16
+    ser.flush()
+    ser.readline()
+    while True:
+        line = str(ser.readline(), encoding='utf-8', errors='ignore')
+        s = line.split(":")
+        if s[0] not in sensors:
+            print("Adding sensor id: " + s[0])
+            sensors[s[0]] = s[1]
+        else:
+            if loops > 0:
+                loops -= 1
+            else:
+                break
+    ser.close()
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
