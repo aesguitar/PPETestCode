85bc1806f1abc3b5f5ed9b6018a776859d0731ff
aesguitar <aesguitar@gmail.com>
2021-03-18 15:14:24 -0400

    Added Arduino Code
    basic arduino code for reading temp sensors

---
+++ Code/temp_monitor.ino
@@ -0,0 +1,36 @@
+byte read_pins[] = {A0, A1, A2, A3};
+byte num_pins = 4;
+
+//12 bit resolution over 3.3 volts
+//10 mV/degC
+const double conv_fact = 3.3/4096.0*100.0;
+
+void setup() {
+  Serial.begin(9600);
+  analogReadResolution(12);
+
+  byte i = 0;
+  for(i; i < num_pins; i++)
+  {
+    pinMode(read_pins[i], INPUT);
+  }
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  byte i = 0;
+  int reading = 0;
+  double temp = 0.0;
+  String message;
+  
+  for(i; i < num_pins; i++)
+  {
+    reading = analogRead(read_pins[i]);
+    temp = (double) reading * conv_fact;
+    message = String(i) + String(":") + String(temp) + String('\n');
+    Serial.print(message);
+  }
+
+}
